44dc2e157faa8b61bc0c50e40e95975901e2534b
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2021-06-26 01:05:57 -0500

    publish articles and save in firebase storage

--- src/firebase.js
+++ src/firebase.js
@@ -4,22 +4,13 @@ import 'firebase/auth';        // for authentication
 import 'firebase/storage';     // for storage
 import 'firebase/database';    // for realtime database
 
-// const firebaseConfig = {
-//     apiKey: "your-api-key",
-//     authDomain: "your-auth-domain",
-//     projectId: "your-proyect-id",
-//     storageBucket: "your-storage-bucket",
-//     messagingSenderId: "your-messaging-sender-id",
-//     appId: "your-app-id"
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyAqFL-y-zl7oem_YYxl1_fkLt_CkErDFSY",
-    authDomain: "linkedin-clone-f42ed.firebaseapp.com",
-    projectId: "linkedin-clone-f42ed",
-    storageBucket: "linkedin-clone-f42ed.appspot.com",
-    messagingSenderId: "55139261463",
-    appId: "1:55139261463:web:1b33d87a1598100b7e1052"
+    apiKey: "your-api-key",
+    authDomain: "your-auth-domain",
+    projectId: "your-proyect-id",
+    storageBucket: "your-storage-bucket",
+    messagingSenderId: "your-messaging-sender-id",
+    appId: "your-app-id"
 };
 
 const firebaseapp = firebase.initializeApp(firebaseConfig);
